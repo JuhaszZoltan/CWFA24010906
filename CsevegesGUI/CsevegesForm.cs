e9d5ad03e83a3a5f00fd497f34f472c31ddfefee
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2024-01-11 13:47:06 +0100

    updt

--- CsevegesGUI/CsevegesForm.cs
+++ CsevegesGUI/CsevegesForm.cs
@@ -5,12 +5,49 @@ namespace CsevegesGUI
 {
     public partial class CsevegesForm : Form
     {
-        private List<Beszelgetes> _beszelgetesek = new();
+        private List<Beszelgetes> beszelgetesek = new();
 
         public CsevegesForm()
         {
             InitializeComponent();
             InitNonStatic();
+
+            this.Load += BeszelgetesekRtbFill;
+            cbxKezdemenyezok.SelectedIndexChanged += BeszelgetesekRtbFill;
+            cbxFogadok.SelectedIndexChanged += BeszelgetesekRtbFill;
+
+        }
+
+        private void BeszelgetesekRtbFill(object sender, EventArgs e)
+        {
+            rtbCsevegesek.Text = string.Empty;
+
+            if (cbxKezdemenyezok.SelectedItem.ToString() == cbxFogadok.SelectedItem.ToString())
+            {
+                rtbCsevegesek.Text = "Nem történt beszélgetés!";
+            }
+            else
+            {
+                var szurtLista = beszelgetesek.Where(
+                    b => b.Kezdemenyezo == cbxKezdemenyezok.SelectedItem.ToString()
+                    && b.Fogado == cbxFogadok.SelectedItem.ToString()).ToList();
+
+                if (szurtLista.Count == 0)
+                {
+                    rtbCsevegesek.Text = "Nem történt beszélgetés!";
+                }
+                else
+                {
+                    int id = 1;
+                    foreach (var b in szurtLista)
+                    {
+                        rtbCsevegesek.Text += $"{id}. " +
+                            $"{b.Kezdet:yy.MM.dd-HH:mm:ss} --> " +
+                            $"{b.Veg:yy.MM.dd-HH:mm:ss}\n";
+                        id++;
+                    }
+                }
+            }
         }
 
         private void InitNonStatic()
@@ -25,7 +62,7 @@ namespace CsevegesGUI
                 Beszelgetes beszelgetes = new(sr.ReadLine());
                 kezd.Add(beszelgetes.Kezdemenyezo);
                 fogad.Add(beszelgetes.Fogado);
-                _beszelgetesek.Add(beszelgetes);
+                beszelgetesek.Add(beszelgetes);
             }
 
             cbxKezdemenyezok.Items.AddRange(kezd.OrderBy(x => x).ToArray());
